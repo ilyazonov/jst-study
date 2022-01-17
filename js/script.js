48cf0006aa839a0c2e18d890b8c1ab8c55727d7a
ilya <ilyazonov@gmail.com>
2022-01-18 00:37:48 +0100

    homework cycles

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let numberIfFilms;
 numberIfFilms = prompt("Сколько фильмов посмотрели?", "");
 console.log(numberIfFilms);
@@ -10,13 +12,23 @@ personalMovieDB = {
     'genres':[],
     'privat':false
 };
-const m1 = prompt('Назовите фильм', '');
-const o1 = prompt('Оцените', '');
-const m2 = prompt('Назовите фильм', '');
-const o2 = prompt('Оцените', '');
+// const m1 = prompt('Назовите фильм', '');
+// const o1 = prompt('Оцените', '');
+// const m2 = prompt('Назовите фильм', '');
+// const o2 = prompt('Оцените', '');
 
-personalMovieDB.movies = {m1:o1, m2:o2};
+for (let i = 1; i<3; i++){
+    const m = prompt('Назовите фильм', ''),
+          o = prompt('Оцените', '');
+    if (m == '' || o == '' || m == null || o == null || m.length > 10 || o.length > 10) {
+        console.log('Fu');
+        i = 1;
+    }
+    personalMovieDB.movies[m] = o;
+}
+
+// personalMovieDB['movies'] = {m1:o1, m2:o2};
+// personalMovieDB.movies[m1] = o1;
+// personalMovieDB.movies[m2] = o2;
 
 console.log(personalMovieDB);
-console.log(m1);
-console.log(m2);
