595674aa9832ffd7841920f1e24e1863cc8aa708
ilya <ilyazonov@gmail.com>
2022-01-17 23:32:40 +0100

    homework moviesDB my version

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log(1);
+let numberIfFilms;
+numberIfFilms = prompt("Сколько фильмов посмотрели?", "");
+console.log(numberIfFilms);
+
+let personalMovieDB;
+personalMovieDB = {
+    'count':numberIfFilms,
+    'movies':{},
+    'actors':{},
+    'genres':[],
+    'privat':false
+};
+const m1 = prompt('Назовите фильм', '');
+const o1 = prompt('Оцените', '');
+const m2 = prompt('Назовите фильм', '');
+const o2 = prompt('Оцените', '');
+
+personalMovieDB.movies = {m1:o1, m2:o2};
+
+console.log(personalMovieDB);
+console.log(m1);
+console.log(m2);
